e2ba371354ecaa34f058dbe8df0a8f4ed054eb84
Leonardo Santana <leonardo_san902@hotmail.com>
2019-05-19 18:25:58 -0300

    Google deu certo

--- Proxy/Proxy.py
+++ Proxy/Proxy.py
@@ -17,17 +17,21 @@ while True:
     print ('Ready to serve...')
     clientSock, enderecoClient = serverSock.accept()
     print ('Received a connection from: ', enderecoClient)
-   
-    message = str(clientSock.recv(bufferSize).decode())
+    message = clientSock.recv(bufferSize)
     print(message)
     # Extract the filename from the given message
-    if message!= '':
+    if message!= b'':
         
-        message.split(' ')[1]
-        filename = message.split()[1].partition("/")[2]
+        message.split(b' ')[1]
+        filename = message.split()[1].partition(b"/")[2]
         print (filename)
-        fileExist = "false"
-        filetouse = "/" + filename
+        
+        if filename.startswith(b'www'):
+            x=0
+        else:
+            x=1
+        fileExist = b"false"
+        filetouse = b"/" + filename
         print(filetouse)
         
         try:
@@ -49,45 +53,56 @@ while True:
             # Error handling for file not found in cache
         except IOError:
             print ('File Exist: ', fileExist)
-            if fileExist == "false":
+            if fileExist == b"false":
                 # Create a socket on the proxyserver
                 print ('Creating socket on proxyserver')
                 c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-                hostn = filename.replace("www.", "", 1)
+                hostn = filename.replace(b"www.", b"", 1)
                 print ('Host Name: ', hostn)
                 try:
                     # Connect to the socket to port 80
-                    c.connect((hostn, 80))
-                    print ('Socket connected to port 80 of the host\n')
-                    
-                    filename= filename.encode('utf-8')
-                    print(b'GET / HTTP/1.1\r\nHost: '+filename+b'\r\nConnection: keep-alive\r\n\r\n')
-                    c.sendall(b'GET / HTTP/1.1\r\nHost: '+filename+b'\r\nConnection: keep-alive\r\n\r\n')
-                    # Create a temporary file on this socket and ask port 80
-                    # for the file requested by the client 
-                    #fileobj = c.makefile('r', None)
-                    #fileobj.write("GET " + "http://" + filename+ " HTTP/1.1\r\n")
-                   
-                   
-                        
-                  
-                    messageSite = c.recv(bufferSize)
-   
-                    while True:
-                            messageSite += c.recv(bufferSize)
+                    if(x==0):                       
+                        c.connect((hostn, 80))
+                        originalHost = hostn
+                        print ('Socket connected to port 80 of the host\n')
+                        print(b'GET / HTTP/1.1\r\nHost: '+filename+b'\r\nConnection: keep-alive\r\n\r\n')
+                        c.sendall(b'GET / HTTP/1.1\r\nHost: '+filename+b'\r\nConnection: keep-alive\r\n\r\n')
+                        messageSite = c.recv(bufferSize)
+                        print(3)
+                        while True:
+                                messageSite += c.recv(2000)
                                                           
-                            if messageSite.endswith(b'\r\n0\r\n\r\n'):
-                             break
-                                     
-                 
+                                if messageSite.endswith(b'\r\n0\r\n\r\n'):
+                                     break
+                       
+                    else :
+                        c.connect((originalHost, 80))
+                        print(b'GET '+ filetouse +b' HTTP/1.1\r\nHost: www.'+originalHost+b'\r\nConnection: keep-alive\r\n\r\n')
+                        c.sendall(b'GET '+ filetouse +b' HTTP/1.1\r\nHost: www.'+originalHost+b'\r\nConnection: keep-alive\r\n\r\n')
+                        messageSite = c.recv(5482)
+                        while True:                     
+                           try:
+                                messageSite += c.recv(5482)
+                                print(messageSite)
+                                if messageSite.endswith(b'\r\n0\r\n\r\n'):
+                                     break
+                           except socket.timeout:
+                               break
+                              
+                                                      
+                            
+                                  
+                    print(2)
+                    
+              
                     print(messageSite)
-  
-                    tmpFile = open(b"./" + filename, "wb")
+                    tmpFile = open(b"./" + hostn, "ab")
                     tmpFile.write(messageSite)
                     clientSock.sendall(messageSite)
-
-                except:
-                    print ('Illegal request')
+                    messageSite=b''
+                except Exception as e: 
+                    print(e)
+                    
 
             else:
                 # HTTP response message for file not found
@@ -95,6 +110,6 @@ while True:
                 print ('File Not Found...Stupid Andy')
                 a = 2
         # Close the socket and the server sockets
-        clientSock.close()
+        
 
 # Do stuff here
